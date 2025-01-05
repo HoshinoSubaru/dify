379c01ebb2c569b06e43ff7aa128e7d625b910b5
HoshinoSubaru <subaru@urlounge.co.jp>
2025-01-05 12:59:08 +0900

    package.json.js

---
+++ line-reservation-bot/package.json.js
@@ -0,0 +1,14 @@
+{
+  "name": "line-reservation-bot",
+  "version": "1.0.0",
+  "description": "リファスタの予約用LINEボット",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@line/bot-sdk": "^7.4.0",
+    "express": "^4.17.1",
+    "dotenv": "^16.0.3"
+  }
+}
